250f1c3e80c782baf36a0657273f5c8a0ff840a3
aodag <aodagx@gmail.com>
2013-12-11 01:00:58 +0900

    convert simple types

--- rebecca/app/admin/sqla.py
+++ rebecca/app/admin/sqla.py
@@ -1,5 +1,6 @@
 import itertools
 import colander as c
+from sqlalchemy import types
 from sqlalchemy.orm import class_mapper
 from sqlalchemy.inspection import inspect
 from zope.interface import implementer
@@ -14,11 +15,36 @@ class SQLAModelAdmin(object):
         self.schema = create_schema(model)
 
 
-def create_schema(model):
+
+default_type_map = {
+    types.String: c.String,
+    types.Integer: c.Integer,
+    types.Unicode: c.String,
+    types.Date: c.Date,
+    types.DateTime: c.DateTime,
+}
+
+
+class DefaultTypeMapper(object):
+    def __init__(self):
+        self.mapps = default_type_map
+
+    def __call__(self, typ):
+        """ convert from sqla type to colander schema type"""
+        for col_type, colander_type in default_type_map.items():
+            if isinstance(typ, col_type):
+                return colander_type
+
+
+default_type_mapper = DefaultTypeMapper()
+
+
+def create_schema(model, schema_type_mapper=default_type_mapper):
     mapper = class_mapper(model)
 
     schema = c.MappingSchema()
     for col in mapper.columns:
-        schema.add(c.SchemaNode(c.Int,
+        typ = getattr(col.type, 'impl', col.type)
+        schema.add(c.SchemaNode(schema_type_mapper(typ),
                                 name=col.name))
     return schema
